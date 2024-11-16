f4f01039c44d66694b4a03f4dead4b1e4e880a15
Rajasekhar607 <luke240294@gmail.com>
2024-11-16 12:29:00 +0530

    Update BajajTest.java

--- sampleproject/src/test/java/bikes/BajajTest.java
+++ sampleproject/src/test/java/bikes/BajajTest.java
@@ -4,6 +4,7 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class BajajTest {
+	// class is modified in github
 	@Test (groups ="smoke")
 	public void demo() {
 		Reporter.log("Bajaj class executed",true);
