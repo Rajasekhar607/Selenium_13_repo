05ce7c0d0721c1c435bc115051e316136f437d78
gunas <gunas@LAPTOP-GEIVF1FS>
2024-11-18 12:46:44 +0530

    SyncFork class added

---
+++ sampleproject/src/test/java/Fork/SyncFork.java
@@ -0,0 +1,12 @@
+package Fork;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SyncFork {
+	@Test
+	public void demo() {
+		Reporter.log("SyncFork class added");
+	}
+
+}
